974a57b7b7e23570b95b6a524f71c358e34ec627
RyoYamada <lewaden6@gmail.com>
2026-04-01 23:55:51 +0300

    final version

--- IsKPeriodic/Main.cpp
+++ IsKPeriodic/Main.cpp
@@ -4,35 +4,35 @@ using namespace std;
 
 bool IsKPeriodic(const string& str, int k)
 {
-    //Проверка корректности входных данных
+    // Проверка корректности входных данных
     if (k <= 0)
     {
         return false;
     }
 
-    //Вычисляем длину строки вручную
+    // Вычисляем длину строки вручную
     int len = 0;
     while (str[len] != '\0')
     {
         len++;
     }
 
-    //Если строка пуста
+    // Если строка пуста
     if (len == 0)
     {
         return false;
     }
 
-    //Если длина строки не кратна k, строка не может быть кратной
+    // Если длина строки не кратна k, строка не может быть кратной
     if (len % k != 0)
     {
         return false;
     }
 
-    //Проверяем каждый блок длины k
+    // Проверяем каждый блок длины k
     for (int i = k; i < len; i += k)
     {
-        //Сравниваем блоки посимвольно
+        // Сравниваем блоки посимвольно
         for (int j = 0; j < k; j++)
         {
             if (str[j] != str[i + j])
@@ -46,5 +46,25 @@ bool IsKPeriodic(const string& str, int k)
 
 int main()
 {
+    cout << "Введите строку: ";
+    string str;
+    cin >> str;
+    cout << endl;
+
+    cout << "Введите кратность: ";
+    int k = 0;
+    cin >> k;
+    cout << endl;
+
+    bool kr = IsKPeriodic(str, k);
+
+    if (kr)
+    {
+        cout << "Строка: " << str << " Кратна " << k << endl;
+    }
+    else
+    {
+        cout << "Строка: " << str << " Не кратна " << k << endl;
+    }
     return 0;
 }
